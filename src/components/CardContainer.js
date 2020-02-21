e8b8abb7feb939d154d9ace7e36976dfafb2b0db
zoe <zldpepi@gmail.comgit config --global user.name Zoe>
2020-02-20 20:39:15 -0500

    mvp

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,20 +1,40 @@
 import React from "react"
+import styled from "styled-components"
+
 
 const PhotoOfTheDay = ({ date, title, url, explanation}) => {
-    
-  return (
-    <section class="card">
-      <div>
-        <img src={url} alt='Nasa'>
-          </img>
-      </div>
-      <h3>Date: {date}</h3>
-      <h3>{title}</h3>
-      <div>
-        <p>{explanation}</p>
-      </div>
-    </section>
-  );
-};
+  const Card = styled.div`
+  width: 50%;
+  height: 50%;
+  border-radius: 24px;
+  margin: 0 auto;
+  padding: 5% 5%;
+  background-color: white;
+  color: black;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 5px 5px 15px 5px black;
+`;
+  const Image = styled.img`
+  width: 100%;
+`;
+  const H1 = styled.h1`
+  font-family: 'Brush Script MT', cursive;
+  font-size: 4rem;
+  `
 
-export default PhotoOfTheDay;
+  return (
+      <Card>
+        <div>
+          <Image src={url} alt='Nasa' />
+        </div>
+        <h3>Date: {date}</h3>
+        <H1>{title}</H1>
+        <div>
+          <p>{explanation}</p>
+        </div>
+      </Card>
+    );
+  };
+
+  export default PhotoOfTheDay
